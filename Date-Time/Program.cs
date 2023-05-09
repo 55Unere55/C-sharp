0d2c18109e06c1530e519c78fb64816797804532
Tamara Iloka <114917818+55Unere55@users.noreply.github.com>
2023-05-09 21:26:11 +0100

    Date+Time

---
+++ Date-Time/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Date_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //shows current Date and Time
+            Console.WriteLine("The current Time is: " + DateTime.Now);
+            //Ask user for number
+            Console.WriteLine("\nEnter any Number to see whicht time will be by then: ");
+            float answer = float.Parse(Console.ReadLine());
+            //shows time AFTER user input
+            Console.WriteLine("{0} + { 1} hour(s) = { 2}", DateTime.Now, answer, DateTime.Now.AddHours(answer));
+        }
+    }
+}
