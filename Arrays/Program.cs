65c44c3a307b18b08a9d1c5f65cbdbeb80448141
Tamara Iloka <114917818+55Unere55@users.noreply.github.com>
2023-05-09 21:54:55 +0100

    create arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5];
+            int[] array1 = new int[] { 1, 3, 5, 7, 9, };
+            Console.WriteLine("Please give a number in: ");
+            int = index.Console.ReadLine();
+            string[] stringArray = new string[6];
+            string[] weekDays = new string[] { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+        }
+    }
+}
