2ffd1e28e34ae18fc6ffbf5789a596490ea0cf5d
Tamara Iloka <114917818+55Unere55@users.noreply.github.com>
2023-05-10 21:24:30 +0100

    update4.2

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -9,17 +9,44 @@ namespace Arrays
             int[] array = new int[5];
             int[] array1 = new int[] { 1, 3, 5, 7, 9, };
             Console.WriteLine("Please give a number in: ");
-            int index = Convert.ToInt32(Console.ReadLine());
-            if (index > 4)
-                Console.WriteLine("You entered a wrong number, Please enter a number between 0 and 4.");
+
+            try
             {
-            Console.WriteLine("The value at the index you entered is: " + array1[index]);
-           
-                string[] stringArray = new string[6];
-                string[] weekDays = new string[] { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
-                int index1 = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("The value at the index you entered is: " + stringArray[index1]);
+                int index = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("The value at the index you entered is: " + array1[index]); }
+
+
+
+            catch
+            {
+                Console.WriteLine("You entered a wrong number, Please enter a number between 0 and 4.");
+
+
+                {
+                    int index = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("The value at the index you entered is: " + array1[index]);
+                    Console.ReadLine();
+
+                    string[] stringArray = new string[6];
+                    string[] weekDays = new string[] { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+
+                    try
+                    {
+                        int index1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("The value at the index you entered is: " + weekDays[index1]);
+                    }
+
+                    catch
+                    {
+                        Console.WriteLine("You entered a wrong number, Please enter a number between 0 and 5.");
+                        int index1 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("The value at the index you entered is: " + weekDays[index1]);
+                        Console.ReadLine();
+                    }
+
+
+                    }
+                }
             }
-        }
     }
 }
