c1fddbea4e77bfffbff2be182e9a755ea4e684ba
Tamara Iloka <114917818+55Unere55@users.noreply.github.com>
2023-05-10 21:07:36 +0100

    update 5.0

---
+++ variables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             void MathOp(int dividend)
+            {
+                Console.WriteLine("Please enter a number: ");
+                int MathOp = Convert.ToInt32(Console.ReadLine());
+                int quotient = MathOp  / 2;
+                Console.WriteLine("Your number divided by two is " + quotient);
+            }
+
+        }
+    }
+
+}
+    
