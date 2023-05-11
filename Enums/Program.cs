dc960d4a2d7ad37f6cc314c0a028b8229df9b8c4
Tamara Iloka <114917818+55Unere55@users.noreply.github.com>
2023-05-11 12:03:51 +0100

    Enums 1.0

---
+++ Enums/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Enums
+{
+    class Program
+    {
+
+    }
+    public enum MonthsOfTheYear
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December,
+    }
+    static void Main(string[] args);
+}
+
+            bool isValid = false;
+            while(!isValid)
+            {
+                try
+                {
+                    Console.WriteLine("Enter the current Month of the Year");
+                    string month = Console.ReadLine();
+                    MonthsOfTheYear month = (MonthsOfTheYear)Enum.Parse(typeof(MonthsOfTheYear), monthInput);
+                    Console.WriteLine("Have a nive " + month);
+                    Console.ReadLine();
+                    isValid = true;
+                }
+                { 
+                catch (ArgumentException execute) {
+                
+                Console.WriteLine("Please enter an actual Month of the Year");
+            }  
+            }
+        }
+        }
+    }
+
