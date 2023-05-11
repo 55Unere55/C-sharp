2cb0bef7dc7df8658d8c38b844716757ac6cf3de
Tamara Iloka <114917818+55Unere55@users.noreply.github.com>
2023-05-11 13:16:43 +0100

    Class-Method 1.0

---
+++ Class_Method/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Class_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             void MathOp(int dividend)
+            {
+                Console.WriteLine("Please enter any number: ");
+                int div = Convert.ToInt32(Console.ReadLine());                                      
+                int quotient = div / 2;
+                Console.WriteLine("Your number divided by two is " + quotient);
+            }
+
+        }
+    }
+}
---
+++ Class_Method/Program1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Method
+{
+    class Program1
+    {
+        void MathOps(int product)
+        {
+            Console.WriteLine("Please enter any number: ");
+            int pro = Convert.ToInt32(Console.ReadLine());
+            int pro2 = 2;
+            int product = pro * pro2;
+            Console.WriteLine("Your number multiplied by two is " + product);
+        }
+
+    }
+}
+
