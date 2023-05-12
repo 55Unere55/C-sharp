dd27e3efc4df707f9b024eb579faebf2a87eec1a
Tamara Iloka <114917818+55Unere55@users.noreply.github.com>
2023-05-12 13:02:50 +0100

    update 5.3

---
+++ Math-Methods-2/Operations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Math_Methods_2
+{
+    class Operations
+    {
+        
+        Console.WriteLine("Please give in a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+        int difference = num1 - 7;
+        Console.WriteLine(difference);
+
+    }
+}
