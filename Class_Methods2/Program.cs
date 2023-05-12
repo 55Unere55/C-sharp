faa08fa5ebe87fd3cac442a638fd7b850a66f3ae
Tamara Iloka <114917818+55Unere55@users.noreply.github.com>
2023-05-12 13:13:00 +0100

    update 5.4

---
+++ Class_Methods2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Clss_Methods2
+{ 
+        public static Main(string[] args)
+    { 
+    
+
+    public class MathOp       
+    {
+        void Math(int addision)
+        {
+            Console.WriteLine("Please enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a second number in if you want: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int total = num1 + (num2) + 9;
+            Console.WriteLine("your number(s) + 9 is: " + total);
+        Console.ReadLine();
+        }
+
+    }
+}
+        
+    
+
