835445f33550a5783c0c9f7dda55a81ff04d4be1
Tamara Iloka <114917818+55Unere55@users.noreply.github.com>
2023-05-12 12:23:42 +0100

    update 5.1

---
+++ Class_Methods1/Method1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Methods1
+{
+    class Method1
+    {
+
+        
+            void MathOps(int prct)
+            {
+                Console.WriteLine("Please enter any number: ");
+                int pro = Convert.ToInt32(Console.ReadLine());
+                int pro2 = 2;
+                int product = pro * pro2;
+                Console.WriteLine("Your number multiplied by two is " + product);
+                Console.ReadLine();
+            }
+
+        }
+    }
+
+
---
+++ Class_Methods1/Method2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Methods1
+{
+    class Method2
+    {
+        static void Main(string[] args)
+        {
+            void MathOps(decimal dif)
+            {
+                Console.WriteLine("Please enter any number: ");
+                decimal dec = Convert.ToInt32(Console.ReadLine());
+                int num = 2;
+                decimal difference = dec - num;
+                Console.WriteLine("Your number - two is " + difference);
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
+
