d96164614e142c72647c0d1ee616e791a0b792e3
Tamara Iloka <114917818+55Unere55@users.noreply.github.com>
2023-05-22 10:40:03 +0100

    update Methods4.0

---
+++ Methods/Meth.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+   public class Meth
+    {
+        public int AddTwoNumbers()
+        {
+            int num = 34;
+            return num1 + num2;
+        }
+        public int MultiplyTwoNumbers()
+        {
+            int num3 = 13;
+            return num3 * num4;
+
+        }
+        public int SubstractTwoNumbers()
+        {
+            int num5 = 50;
+            return num5 - num6;
+
+        }
+    }
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Fun Counting Room.");
+            
+            Console.WriteLine("Please enter your first Number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Please enter a second Number: ");
+            int num4 = Convert.ToInt32(Console.ReadLine()); 
+
+
+
+    }}
+}
