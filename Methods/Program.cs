a693a61b36e3a58a5ee06c6952392267f98a6415
Tamara Iloka <114917818+55Unere55@users.noreply.github.com>
2023-05-26 11:34:38 +0100

    update 1.0

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -8,28 +8,25 @@ namespace Methods
         {
             Console.WriteLine("Welcome to the Fun Counting Room.");
 
-            Meth object = new Meth();
-            object.AddTwoNumbers();
+            Meth myobject = new Meth();            
             Console.WriteLine("Please enter your first Number: ");
             int num2 = Convert.ToInt32(Console.ReadLine());
-            int total = num1 + num2;
+            int total = 34 + num2;
             Console.WriteLine(total);
             Console.ReadLine();
 
-            Meth object = new Mwth();
-            object.MultiplyTwoNumbers();
+            Meth myobject1 = new Meth();           
         Console.WriteLine("Please enter a second Number: ");
             int num4 = Convert.ToInt32(Console.ReadLine());
-            int product = num3 * num4;
+            int product = 13 * num4;
             Console.WriteLine(product);
             Console.ReadLine();
 
 
-            Meth Object = new Meth();
-            object.SubstractTwoNumbers();
+            Meth myObject2 = new Meth();           
             Console.WriteLine("Please give one last Number in: ");
-            int num5 = Convert.ToInt32(Console.ReadLine());
-            int difference = num5 - num6;
+            int num6 = Convert.ToInt32(Console.ReadLine());
+            int difference = 50 - num6;
             Console.WriteLine(difference);
             Console.ReadLine();
     }}
