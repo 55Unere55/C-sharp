7cb24cd5447ad9e7b71f0c9b2a5fcaa77bb6e3ee
Tamara Iloka <114917818+55Unere55@users.noreply.github.com>
2023-05-30 10:50:30 +0100

    update 5.0

--- Exception handling/Program.cs
+++ Exception handling/Program.cs
@@ -4,7 +4,7 @@ namespace Exception_handling
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             DateTime myDate = DateTime.Now;
             Console.WriteLine("How old are you ? ");
@@ -12,8 +12,29 @@ namespace Exception_handling
             DateTime newDate = myDate.AddYears(-age);
             int newYear = newDate.Year;
             Console.WriteLine(newYear);
-        }
 
+            if (age == 0)
+            {
+                try
+                {
+                    Console.WriteLine("Please enter a number not NULL !");
+                    Console.ReadLine();
+                    return;
+                }
+                
+                catch
+                {
+                    Console.WriteLine("Something went wrong, please try again to enter your Age: ");
+                    return;
+                }
+                
+                
+            }
         }
     }
+}
+
+
+        
+    
 
