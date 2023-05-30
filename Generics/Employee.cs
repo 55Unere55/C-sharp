9edac48c0fbd3dd43d25e125bba0f7142821c2e4
Tamara Iloka <114917818+55Unere55@users.noreply.github.com>
2023-05-30 12:04:51 +0100

    update to Interface

--- Generics/Employee.cs
+++ Generics/Employee.cs
@@ -4,18 +4,30 @@ using System.Text;
 
 namespace Generics
 {
-    public class Employee<T>
+    public class Employee<T>: IQuittable
     {
+        bool isWorking { get; set; }
 
         // define a variable of type T 
         public T data;
 
         // define a constructor of Employee class 
-        public Employee(T data)
+        public Employee(T data) 
         {
             this.data = data;
             Console.WriteLine("Passed on: " + this.data);
         }
+
+        
+       public void Quit(Employee<string> employee)
+        {
+            if(!isworking == false)
+            {
+                IQuit.Employee<string>.Remove(employee);
+                return;
+            }
+        }
+
     }
 }
 
---
+++ Generics/IQuittable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    interface IQuittable
+    {
+        void Quit(Employee<string> employee);
+    }
+    public void IQuit(Employee<string>)
+    {
+      Employee<String> IQuit = new Employee<string>()
+      {
+          if (!isworking == false)
+        {
+            IQuit.Employee.Remove(employee);
+            return;
+        }
+    }
+    }
+}
+
