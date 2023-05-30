f3221defa48dbe1473a82743a9258abd192d0d25
Tamara Iloka <114917818+55Unere55@users.noreply.github.com>
2023-05-30 10:03:42 +0100

    create Exception handling

---
+++ Exception handling/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exception_handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime myDate = DateTime.Now;
+            Console.WriteLine("How old are you ? ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            DateTime newDate = myDate.AddYears(-age);
+            int newYear = newDate.Year;
+            Console.WriteLine(newYear);
+        }
+
+        }
+    }
+
