132b1aa718a48afa9531887695660351ee70829e
Tamara Iloka <114917818+55Unere55@users.noreply.github.com>
2023-05-30 22:08:36 +0100

    11

--- Methods/Meth.cs
+++ Methods/Meth.cs
@@ -6,19 +6,19 @@ namespace Methods
 {
    public class Meth
     {
-        public int AddTwoNumbers( int num2)
+        public int AddTwoNumbers(int num2)
         {
             
             int num1 = 34;
             int total = num1 + num2;
-            return num1 + num2;
+            Console.WriteLine(total);
         }
         public int MultiplyTwoNumbers(int num4)
         {
           
             int num3 = 50;
             int product = num3 * num4;
-            return num3 * num4;
+            Console.WriteLine(product);
 
         }
         public int SubstractTwoNumbers(int num6)
@@ -26,7 +26,7 @@ namespace Methods
             
             int num5 = 13;
             int difference = num5 - num6;
-            return num5 - num6;
+            Console.WriteLine(difference);
 
         }
     }
