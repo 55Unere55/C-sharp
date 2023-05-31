e380607bfbe9ee0ba6b4953d8aaa064cf46bfbd4
Tamara Iloka <114917818+55Unere55@users.noreply.github.com>
2023-05-31 21:38:04 +0100

    13

---
+++ IO/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+
+namespace IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, please enter any number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            using (StreamWriter file = new StreamWriter(@"C\Users\tamar\Logs\log.txt", true))
+            {
+                file.Write(num);
+            }
+        }
+    }
+}
